6cd26b863bd34d563dc34f3d0b539dafe53d546e
RashidAhmad111 <159822331+RashidAhmad111@users.noreply.github.com>
2024-02-13 10:14:40 +0530

    Add files via upload

---
+++ inedex.js
@@ -0,0 +1,13 @@
+button=document.getElementById("sign").onclick=function(){
+  alert("I LOVE YOU RASHID  ")
+  
+}
+button=document.getElementById("r1").onclick=function(){
+  alert("VEG PIZAA  ")
+  alert("MRP 240 ")
+  alert("40%OFF")
+  alert("180 MRP")
+}
+button=document.getElementById("sign").onclick=function(){
+  alert("I LOVE YOU RASHID  ")
+}
